130ca925ccad9d0ba10058ba31f1304261872f67
Gabriel Augusto da Silva Xavier <gasxavier17@gmail.com>
2025-05-12 15:07:08 -0400

    Add files via upload

--- Imc.py
+++ Imc.py
@@ -1,27 +1,9 @@
+nome=input("Insira seu nome: ")
+peso=int(input("Insira seu peso: "))
+altura=float(input("Insira sua altura (em metros): "))
+imc=0
 
-def calculadora_imc(pessoa):
-    imc = pessoa["peso"]/(pessoa["altura"]*pessoa["altura"])
-    resultado = (f"O IMC {pessoa["nome"]} é {imc:.2f}")
-    saudavel = 18.5 <imc < 25
-
-    return{
-        "nome": pessoa["nome"],
-        "imc": round(imc,2),
-        "resultado": resultado,
-        "saudavel": saudavel,
-    }
-
-pessoa1={
-    "nome": "José",
-    "peso": 110,
-    "altura": 1.75
-}
-
-pessoa2={
-    "nome": input("Digite o nome da segunda pessoa:\n"),
-    "peso": int(input("Digite o peso da segunda pessoa:\n")),
-    "altura": float(input("Digite a altura da segunda pessoa em metros:\n"))
-}
-
-print(calculadora_imc(pessoa1))
-print(calculadora_imc(pessoa2))
+def CALCULAAR_IMC(nome):
+    imc= peso/(altura*altura)
+    print(f"{nome}, o seu imc é {imc}")
+imc=CALCULAAR_IMC(nome)
