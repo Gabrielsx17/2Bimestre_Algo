4fd01abe50803946b34f9130df45546ac2162041
Gabriel Augusto da Silva Xavier <gasxavier17@gmail.com>
2025-06-09 13:15:29 -0400

    Add files via upload

--- correio.py
+++ correio.py
@@ -2,7 +2,7 @@ pacotes=("ABC123","XYZ789","DEF456","JKL321","MNO654","PQR987","STU741")
 
 rastreio=("Enviado", "Recebido", "Em Trânsito" ,"Enviado", "Recebido","Em Trânsito","Enviado")
 
-codigos_do_Em_Transito=()
+codigos_do_Em_Transito=[]
 quantidade_Enviado=rastreio.count("Enviado")
 quantidade_Recibido=rastreio.count("Recebido")
 quantidade_Em_Transito=rastreio.count("Em Trânsito")
